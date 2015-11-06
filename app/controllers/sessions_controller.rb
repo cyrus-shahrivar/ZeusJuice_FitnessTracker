896ef3dcec15eebd17dea8539f705af765810285
Cyrus Shahrivar <cyrusshahrivar@Cyruss-MacBook-Pro.local>
2015-11-06 10:35:01 -0500

    heroku changes

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
     user = Member.find_by(username: params[:session][:username])
     if user && user.authenticate(params[:session][:password])
       log_in user
-      redirect_to 'sessions'
+      redirect_to root_path
     else
       flash.now[:danger] = "Invalid username/password combination"
       render 'new'
